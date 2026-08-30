c53fd3919547ef72871ad080ab599f01e6d20483
Rakesh Kumar <rakeshkumar102208@gmail.com>
2026-08-30 16:19:34 +0530

    Add files via upload
    comment in C++

---
+++ comment.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+int main(){
+
+    std:: cout <<"Let's learn about comments";
+
+    //This is a single line comment
+
+    /*This is a type of paired comment.
+    Everything between the delimiters are ignored .
+    Std:: cout<< "This too"; */
+
+    return 0;
+}
