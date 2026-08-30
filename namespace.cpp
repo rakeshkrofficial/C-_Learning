4c02eb01ce8a71a84ad088094519cefebc42594f
Rakesh Kumar <rakeshkumar102208@gmail.com>
2026-08-30 16:17:45 +0530

    Add files via upload

---
+++ namespace.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using std::cout;
+
+int main(){
+    cout <<"Enter any number:";
+
+    int n1=0;
+
+    std::cin >> n1;
+    cout <<"Entered Number is : " <<n1;
+
+    return 0;
+}
