1946688419ba1aeeb37f6d5ac83d1ff72fe1b18a
Rakesh Kumar <rakeshkumar102208@gmail.com>
2026-08-30 16:21:06 +0530

    Add files via upload

---
+++ float_litrl.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    float n,m,o,p;
+
+    n=3.1415;
+    m=3.1415E2;
+    o=3.1415e3;
+    p=3.1415E-1;
+
+    cout << n << endl << m << endl << o << endl << p;
+}
