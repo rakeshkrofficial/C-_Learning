da890cecbe2b8a8bc0cd988004b4e9af4c1bc43f
Rakesh Kumar <rakeshkumar102208@gmail.com>
2026-08-30 16:18:50 +0530

    Add files via upload

---
+++ namespace_1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout <<"Enter any number :"<<endl;
+    int n=0;
+
+    cin >> n;
+
+    cout << "Enter Number is : " << n;
+
+    return 0;
+}
