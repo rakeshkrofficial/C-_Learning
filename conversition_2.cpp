f521f3665bc9e948161c2daa6b3e4a2f6de34f1a
Rakesh Kumar <rakeshkumar102208@gmail.com>
2026-09-02 09:20:49 +0530

    Add files via upload

---
+++ conversition_2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int i=35;
+
+    i=3.14;
+
+    cout<< "i = " << i <<endl;
+
+    double pi=i;
+    cout << "pi = " << pi <<endl;
+
+    return 0;
+}
