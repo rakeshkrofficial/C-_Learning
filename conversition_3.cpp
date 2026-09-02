65297b730ad35949c8cc667a58baf1f889b24c2a
Rakesh Kumar <rakeshkumar102208@gmail.com>
2026-09-02 09:21:08 +0530

    Add files via upload

---
+++ conversition_3.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    unsigned c1=-1;
+    cout << "c1 = "  << c1 <<endl;
+
+    unsigned char c2=-1;  // convert into binary the convert banary to 1's Complement then convert 1's to 2's complement then it has a meaning
+    
+    cout << "c2 = "  << c2 <<endl;
+
+    unsigned char c3=-3;
+    cout << "c3 = "  << c3 <<endl;
+
+    unsigned char c4=-4;
+    cout << "c4 = "  << c4 <<endl;
+
+    return 0;
+}
