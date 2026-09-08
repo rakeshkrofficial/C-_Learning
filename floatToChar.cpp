6c7f875300927d7dbf8448cd69d043cd5908eca5
Rakesh Kumar <rakeshkumar102208@gmail.com>
2026-09-08 16:32:30 +0530

    Add files via upload

---
+++ floatToChar.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    float floatNO = 65.5;
+    char charvalue = (char)floatNO;
+    char charvlu = char(floatNO);
+
+    cout<<charvalue<<endl<< charvlu;
+}
