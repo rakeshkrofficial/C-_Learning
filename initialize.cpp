f223cb80ce1700fd1bc1bbcf67cfba3a0209eb31
Rakesh Kumar <rakeshkumar102208@gmail.com>
2026-09-09 22:55:16 +0530

    Add files via upload

---
+++ initialize.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    // declaration
+    int arr[100];
+
+    // initialisation
+
+    int brr[5] = {10,20,30,40};
+    int crr[] = {10,20,30};
+
+    int multipleOf2[10] = {2,4,6,8,10,12,14,16,18,20};
+
+    
+    
+}
